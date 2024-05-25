8daad7ce90b0728e8168da5b1ff9e62687c1ed4b
Deval <deval135darji@gmail.com>
2024-05-25 17:49:52 +0530

    Add controller for getting user's balance

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,5 +1,41 @@
+const moment = require('moment');
+const Trade = require("../models/Trade");
+
 const CheckBalance = async (req, res) => {
-    res.status(200).json({ message: "Balance" })
+    
+    const { timestamp } = req.body;
+    const formattedTimeStamp = moment(timestamp, "YYYY-MM-DD HH:mm:ss").toDate();
+
+    try {
+        const trades = await Trade.find({
+            utc_time: { $lt: formattedTimeStamp }
+        });
+
+        const balance = {}
+
+        trades.map(trade => {
+            // destructure basecoin, operation and amount
+            const { base_coin, operation, amount } = trade;
+            // console.log(base_coin, operation, amount);
+
+            // add the coin in balance if not present already
+            if (!balance[base_coin]) {
+                balance[base_coin] = 0;
+            }
+
+            // add the amount if it is a buy and subtract if it is a sell
+            if (operation === "Buy") {
+                balance[base_coin] += amount;
+            } else if (operation === "Sell") {
+                balance[base_coin] -= amount;
+            }
+        });
+
+        res.status(200).json(balance)
+    } catch (error) {
+        res.status(400).json(error)
+    }
+
 }
 
 module.exports = { CheckBalance }
