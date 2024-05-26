a8c13e65c4250fca59c7f6dd0920cf62a5fc557e
Deval <deval135darji@gmail.com>
2024-05-26 14:28:38 +0530

    Refactor Multer confifuration into Multer Utils

--- routes/TradeRoute.js
+++ routes/TradeRoute.js
@@ -1,27 +1,8 @@
 const express = require('express');
 const { UploadCSV } = require('../controllers/TradeController');
 const router = express.Router(); 
-const multer = require('multer');
-const fs = require('fs');
-const path = require('path');
 
-// Create the directory for storing the csv files, if it does not exists already
-const csvFiles = path.join(__dirname, '..', 'csvFiles');
-if (!fs.existsSync(csvFiles)) {
-    fs.mkdirSync(csvFiles);
-}
-
-// Configuring multer to store the csv files in the /csvFiles directory
-const csvStorage = multer.diskStorage({
-    destination: function(req,file, cb){ 
-        cb(null, 'csvFiles')
-    },
-    filename: function(req,file,cb){
-        cb(null, Date.now() + '_' + file.originalname);
-    }
-})
-
-const csvFile = multer({ storage: csvStorage}).single('csvFile');
+const csvFile = require('../utils/MulerUtils');
 
 router.post('/upload', csvFile, UploadCSV)
 
---
+++ utils/MulerUtils.js
@@ -0,0 +1,23 @@
+const multer = require('multer');
+const fs = require('fs');
+const path = require('path');
+
+// Create the directory for storing the csv files, if it does not exists already
+const csvFiles = path.join(__dirname, '..', 'csvFiles');
+if (!fs.existsSync(csvFiles)) {
+    fs.mkdirSync(csvFiles);
+}
+
+// Configuring multer to store the csv files in the /csvFiles directory
+const csvStorage = multer.diskStorage({
+    destination: function(req,file, cb){ 
+        cb(null, 'csvFiles')
+    },
+    filename: function(req,file,cb){
+        cb(null, Date.now() + '_' + file.originalname);
+    }
+})
+
+const csvFile = multer({ storage: csvStorage}).single('csvFile');
+
+module.exports = csvFile
